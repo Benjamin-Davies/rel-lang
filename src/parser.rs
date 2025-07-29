24779fd0b1e47af14416fe1eefded53268456313
Benjamin Davies <bentendavies@gmail.com>
2025-07-29 23:09:05 +1200

    refactor: tidy up error reporting

--- src/parser.rs
+++ src/parser.rs
@@ -45,39 +45,46 @@ fn parse_inner<T>(
         Vec::new()
     };
 
-    handle_errors(filename, src, errs, parse_errs);
+    handle_errors(
+        filename,
+        src,
+        errs.into_iter()
+            .map(|e| e.map_token(|c| c.to_string()))
+            .chain(
+                parse_errs
+                    .into_iter()
+                    .map(|e| e.map_token(|tok| tok.to_string())),
+            ),
+    );
 
     Err(Error)
 }
 
-fn handle_errors(filename: &str, src: &str, errs: Vec<Rich<char>>, parse_errs: Vec<Rich<Token>>) {
+fn handle_errors<'src>(
+    filename: &str,
+    src: &str,
+    errs: impl IntoIterator<Item = Rich<'src, String>>,
+) {
     let filename = filename.to_owned();
     let src = src.to_owned();
-    errs.into_iter()
-        .map(|e| e.map_token(|c| c.to_string()))
-        .chain(
-            parse_errs
-                .into_iter()
-                .map(|e| e.map_token(|tok| tok.to_string())),
-        )
-        .for_each(|e| {
-            Report::build(ReportKind::Error, (filename.clone(), e.span().into_range()))
-                .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
-                .with_message(e.to_string())
-                .with_label(
-                    Label::new((filename.clone(), e.span().into_range()))
-                        .with_message(e.reason().to_string())
-                        .with_color(Color::Red),
-                )
-                .with_labels(e.contexts().map(|(label, span)| {
-                    Label::new((filename.clone(), span.into_range()))
-                        .with_message(format!("while parsing this {label}"))
-                        .with_color(Color::Yellow)
-                }))
-                .finish()
-                .print(sources([(filename.clone(), src.clone())]))
-                .unwrap()
-        });
+    for e in errs.into_iter() {
+        Report::build(ReportKind::Error, (filename.clone(), e.span().into_range()))
+            .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
+            .with_message(e.to_string())
+            .with_label(
+                Label::new((filename.clone(), e.span().into_range()))
+                    .with_message(e.reason().to_string())
+                    .with_color(Color::Red),
+            )
+            .with_labels(e.contexts().map(|(label, span)| {
+                Label::new((filename.clone(), span.into_range()))
+                    .with_message(format!("while parsing this {label}"))
+                    .with_color(Color::Yellow)
+            }))
+            .finish()
+            .eprint(sources([(filename.clone(), src.clone())]))
+            .unwrap()
+    }
 }
 
 fn program<'tokens, 'src: 'tokens, I>()
