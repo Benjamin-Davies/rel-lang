9e3fbc5f5205e785c32ce0dca13d9947a9058ca5
Benjamin Davies <bentendavies@gmail.com>
2025-09-03 10:31:15 +1200

    refactor: move split_on_next_var to separate function

--- rel-lang-dd/src/ops.rs
+++ rel-lang-dd/src/ops.rs
@@ -101,12 +101,12 @@ impl Node {
     }
 }
 
-fn eq(lhs: &Rc<node::Inner>, rhs: &Rc<node::Inner>) -> bool {
-    if CacheKey::from(lhs) == CacheKey::from(rhs) {
+fn eq(f: &Rc<node::Inner>, g: &Rc<node::Inner>) -> bool {
+    if CacheKey::from(f) == CacheKey::from(g) {
         return true;
     }
 
-    match (&lhs.kind, &rhs.kind) {
+    match (&f.kind, &g.kind) {
         (node::Kind::True, node::Kind::True) => true,
         (node::Kind::False, node::Kind::False) => true,
         (
@@ -127,144 +127,66 @@ fn eq(lhs: &Rc<node::Inner>, rhs: &Rc<node::Inner>) -> bool {
     }
 }
 
-fn and(cache: &Rc<Cache>, lhs: &Rc<node::Inner>, rhs: &Rc<node::Inner>) -> Rc<node::Inner> {
-    if CacheKey::from(lhs) == CacheKey::from(rhs) {
-        return Rc::clone(lhs);
+fn and(cache: &Rc<Cache>, f: &Rc<node::Inner>, g: &Rc<node::Inner>) -> Rc<node::Inner> {
+    if CacheKey::from(f) == CacheKey::from(g) {
+        return Rc::clone(f);
     }
 
-    match (&lhs.kind, &rhs.kind) {
+    match (&f.kind, &g.kind) {
         (node::Kind::False, _) | (_, node::Kind::False) => cache.false_node(),
-        (_, node::Kind::True) => Rc::clone(lhs),
-        (node::Kind::True, _) => Rc::clone(rhs),
-        (
-            node::Kind::NonTerminal {
-                level: lhs_level,
-                then_child: lhs_then,
-                else_child: lhs_else,
-                cache: _,
-            },
-            node::Kind::NonTerminal {
-                level: rhs_level,
-                then_child: rhs_then,
-                else_child: rhs_else,
-                cache: _,
-            },
-        ) => match lhs_level.cmp(rhs_level) {
-            Ordering::Less => {
-                // Only split the lhs node (which is evaluated first)
-                let new_then = and(cache, lhs_then, rhs);
-                let new_else = and(cache, lhs_else, rhs);
-                cache.get_or_insert(*lhs_level, &new_then, &new_else)
-            }
-            Ordering::Equal => {
-                // Split both nodes (they are evaluated in parallel)
-                let new_then = and(cache, lhs_then, rhs_then);
-                let new_else = and(cache, lhs_else, rhs_else);
-                cache.get_or_insert(*lhs_level, &new_then, &new_else)
-            }
-            Ordering::Greater => {
-                // Only split the rhs node (which is evaluated first)
-                let new_then = and(cache, lhs, rhs_then);
-                let new_else = and(cache, lhs, rhs_else);
-                cache.get_or_insert(*rhs_level, &new_then, &new_else)
-            }
-        },
+        (_, node::Kind::True) => Rc::clone(f),
+        (node::Kind::True, _) => Rc::clone(g),
+        (node::Kind::NonTerminal { .. }, node::Kind::NonTerminal { .. }) => {
+            let (level, fv, fnv, gv, gnv) = split_on_next_var(f, g);
+
+            let new_then = and(cache, fv, gv);
+            let new_else = and(cache, fnv, gnv);
+            cache.get_or_insert(level, &new_then, &new_else)
+        }
     }
 }
 
-fn or(cache: &Rc<Cache>, lhs: &Rc<node::Inner>, rhs: &Rc<node::Inner>) -> Rc<node::Inner> {
-    if CacheKey::from(lhs) == CacheKey::from(rhs) {
-        return Rc::clone(lhs);
+fn or(cache: &Rc<Cache>, f: &Rc<node::Inner>, g: &Rc<node::Inner>) -> Rc<node::Inner> {
+    if CacheKey::from(f) == CacheKey::from(g) {
+        return Rc::clone(f);
     }
 
-    match (&lhs.kind, &rhs.kind) {
+    match (&f.kind, &g.kind) {
         (node::Kind::True, _) | (_, node::Kind::True) => cache.true_node(),
-        (_, node::Kind::False) => Rc::clone(lhs),
-        (node::Kind::False, _) => Rc::clone(rhs),
-        (
-            node::Kind::NonTerminal {
-                level: lhs_level,
-                then_child: lhs_then,
-                else_child: lhs_else,
-                cache: _,
-            },
-            node::Kind::NonTerminal {
-                level: rhs_level,
-                then_child: rhs_then,
-                else_child: rhs_else,
-                cache: _,
-            },
-        ) => match lhs_level.cmp(rhs_level) {
-            Ordering::Less => {
-                // Only split the lhs node (which is evaluated first)
-                let new_then = or(cache, lhs_then, rhs);
-                let new_else = or(cache, lhs_else, rhs);
-                cache.get_or_insert(*lhs_level, &new_then, &new_else)
-            }
-            Ordering::Equal => {
-                // Split both nodes (they are evaluated in parallel)
-                let new_then = or(cache, lhs_then, rhs_then);
-                let new_else = or(cache, lhs_else, rhs_else);
-                cache.get_or_insert(*lhs_level, &new_then, &new_else)
-            }
-            Ordering::Greater => {
-                // Only split the rhs node (which is evaluated first)
-                let new_then = or(cache, lhs, rhs_then);
-                let new_else = or(cache, lhs, rhs_else);
-                cache.get_or_insert(*rhs_level, &new_then, &new_else)
-            }
-        },
+        (_, node::Kind::False) => Rc::clone(f),
+        (node::Kind::False, _) => Rc::clone(g),
+        (node::Kind::NonTerminal { .. }, node::Kind::NonTerminal { .. }) => {
+            let (level, fv, fnv, gv, gnv) = split_on_next_var(f, g);
+
+            let new_then = or(cache, fv, gv);
+            let new_else = or(cache, fnv, gnv);
+            cache.get_or_insert(level, &new_then, &new_else)
+        }
     }
 }
 
-fn xor(cache: &Rc<Cache>, lhs: &Rc<node::Inner>, rhs: &Rc<node::Inner>) -> Rc<node::Inner> {
-    if CacheKey::from(lhs) == CacheKey::from(rhs) {
+fn xor(cache: &Rc<Cache>, f: &Rc<node::Inner>, g: &Rc<node::Inner>) -> Rc<node::Inner> {
+    if CacheKey::from(f) == CacheKey::from(g) {
         return cache.false_node();
     }
 
-    match (&lhs.kind, &rhs.kind) {
-        (node::Kind::True, _) => not(cache, rhs),
-        (node::Kind::False, _) => Rc::clone(rhs),
-        (_, node::Kind::True) => not(cache, lhs),
-        (_, node::Kind::False) => Rc::clone(lhs),
-        (
-            node::Kind::NonTerminal {
-                level: lhs_level,
-                then_child: lhs_then,
-                else_child: lhs_else,
-                cache: _,
-            },
-            node::Kind::NonTerminal {
-                level: rhs_level,
-                then_child: rhs_then,
-                else_child: rhs_else,
-                cache: _,
-            },
-        ) => match lhs_level.cmp(rhs_level) {
-            Ordering::Less => {
-                // Only split the lhs node (which is evaluated first)
-                let new_then = xor(cache, lhs_then, rhs);
-                let new_else = xor(cache, lhs_else, rhs);
-                cache.get_or_insert(*lhs_level, &new_then, &new_else)
-            }
-            Ordering::Equal => {
-                // Split both nodes (they are evaluated in parallel)
-                let new_then = xor(cache, lhs_then, rhs_then);
-                let new_else = xor(cache, lhs_else, rhs_else);
-                cache.get_or_insert(*lhs_level, &new_then, &new_else)
-            }
-            Ordering::Greater => {
-                // Only split the rhs node (which is evaluated first)
-                let new_then = xor(cache, lhs, rhs_then);
-                let new_else = xor(cache, lhs, rhs_else);
-                cache.get_or_insert(*rhs_level, &new_then, &new_else)
-            }
-        },
+    match (&f.kind, &g.kind) {
+        (node::Kind::True, _) => not(cache, g),
+        (node::Kind::False, _) => Rc::clone(g),
+        (_, node::Kind::True) => not(cache, f),
+        (_, node::Kind::False) => Rc::clone(f),
+        (node::Kind::NonTerminal { .. }, node::Kind::NonTerminal { .. }) => {
+            let (level, fv, fnv, gv, gnv) = split_on_next_var(f, g);
+
+            let new_then = xor(cache, fv, gv);
+            let new_else = xor(cache, fnv, gnv);
+            cache.get_or_insert(level, &new_then, &new_else)
+        }
     }
 }
 
-fn not(cache: &Rc<Cache>, node: &Rc<node::Inner>) -> Rc<node::Inner> {
-    match &node.kind {
+fn not(cache: &Rc<Cache>, f: &Rc<node::Inner>) -> Rc<node::Inner> {
+    match &f.kind {
         node::Kind::True => cache.false_node(),
         node::Kind::False => cache.true_node(),
         node::Kind::NonTerminal {
@@ -280,48 +202,22 @@ fn not(cache: &Rc<Cache>, node: &Rc<node::Inner>) -> Rc<node::Inner> {
     }
 }
 
-fn implies(cache: &Rc<Cache>, lhs: &Rc<node::Inner>, rhs: &Rc<node::Inner>) -> Rc<node::Inner> {
-    if CacheKey::from(lhs) == CacheKey::from(rhs) {
+fn implies(cache: &Rc<Cache>, f: &Rc<node::Inner>, g: &Rc<node::Inner>) -> Rc<node::Inner> {
+    if CacheKey::from(f) == CacheKey::from(g) {
         return cache.true_node();
     }
 
-    match (&lhs.kind, &rhs.kind) {
+    match (&f.kind, &g.kind) {
         (node::Kind::False, _) | (_, node::Kind::True) => cache.true_node(),
-        (_, node::Kind::False) => not(cache, lhs),
-        (node::Kind::True, _) => Rc::clone(rhs),
-        (
-            node::Kind::NonTerminal {
-                level: lhs_level,
-                then_child: lhs_then,
-                else_child: lhs_else,
-                cache: _,
-            },
-            node::Kind::NonTerminal {
-                level: rhs_level,
-                then_child: rhs_then,
-                else_child: rhs_else,
-                cache: _,
-            },
-        ) => match lhs_level.cmp(rhs_level) {
-            Ordering::Less => {
-                // Only split the lhs node (which is evaluated first)
-                let new_then = implies(cache, lhs_then, rhs);
-                let new_else = implies(cache, lhs_else, rhs);
-                cache.get_or_insert(*lhs_level, &new_then, &new_else)
-            }
-            Ordering::Equal => {
-                // Split both nodes (they are evaluated in parallel)
-                let new_then = implies(cache, lhs_then, rhs_then);
-                let new_else = implies(cache, lhs_else, rhs_else);
-                cache.get_or_insert(*lhs_level, &new_then, &new_else)
-            }
-            Ordering::Greater => {
-                // Only split the rhs node (which is evaluated first)
-                let new_then = implies(cache, lhs, rhs_then);
-                let new_else = implies(cache, lhs, rhs_else);
-                cache.get_or_insert(*rhs_level, &new_then, &new_else)
-            }
-        },
+        (_, node::Kind::False) => not(cache, f),
+        (node::Kind::True, _) => Rc::clone(g),
+        (node::Kind::NonTerminal { .. }, node::Kind::NonTerminal { .. }) => {
+            let (level, fv, fnv, gv, gnv) = split_on_next_var(f, g);
+
+            let new_then = implies(cache, fv, gv);
+            let new_else = implies(cache, fnv, gnv);
+            cache.get_or_insert(level, &new_then, &new_else)
+        }
     }
 }
 
@@ -339,48 +235,125 @@ pub(crate) fn if_then_else(
         (_, _, node::Kind::True) => or(cache, &not(cache, f), g),
         (_, _, node::Kind::False) => and(cache, f, g),
         (
-            node::Kind::NonTerminal {
-                level: f_level,
-                then_child: f_then,
-                else_child: f_else,
-                cache: _,
-            },
-            node::Kind::NonTerminal {
-                level: g_level,
-                then_child: g_then,
-                else_child: g_else,
-                cache: _,
-            },
-            node::Kind::NonTerminal {
-                level: h_level,
-                then_child: h_then,
-                else_child: h_else,
-                cache: _,
-            },
+            node::Kind::NonTerminal { .. },
+            node::Kind::NonTerminal { .. },
+            node::Kind::NonTerminal { .. },
         ) => {
-            let min_level = f_level.min(g_level).min(h_level);
-
-            let (fv, fnv) = if f_level == min_level {
-                (f_then, f_else)
-            } else {
-                (f, f)
-            };
-            let (gv, gnv) = if g_level == min_level {
-                (g_then, g_else)
-            } else {
-                (g, g)
-            };
-            let (hv, hnv) = if h_level == min_level {
-                (h_then, h_else)
-            } else {
-                (h, h)
-            };
+            let (level, fv, fnv, gv, gnv, hv, hnv) = split_on_next_var3(f, g, h);
 
             let new_then = if_then_else(cache, fv, gv, hv);
             let new_else = if_then_else(cache, fnv, gnv, hnv);
-            cache.get_or_insert(*min_level, &new_then, &new_else)
+            cache.get_or_insert(level, &new_then, &new_else)
         }
     }
+}
+
+fn split_on_next_var<'f, 'g>(
+    f: &'f Rc<node::Inner>,
+    g: &'g Rc<node::Inner>,
+) -> (
+    u64,
+    &'f Rc<node::Inner>,
+    &'f Rc<node::Inner>,
+    &'g Rc<node::Inner>,
+    &'g Rc<node::Inner>,
+) {
+    let node::Kind::NonTerminal {
+        level: f_level,
+        then_child: f_then,
+        else_child: f_else,
+        cache: _,
+    } = &f.kind
+    else {
+        panic!("Expected NonTerminal node for lhs");
+    };
+    let node::Kind::NonTerminal {
+        level: g_level,
+        then_child: g_then,
+        else_child: g_else,
+        cache: _,
+    } = &g.kind
+    else {
+        panic!("Expected NonTerminal node for rhs");
+    };
+
+    match f_level.cmp(g_level) {
+        Ordering::Less => {
+            // Only split the lhs node (which is evaluated first)
+            (*f_level, f_then, f_else, g, g)
+        }
+        Ordering::Equal => {
+            // Split both nodes (they are evaluated in parallel)
+            (*f_level, f_then, f_else, g_then, g_else)
+        }
+        Ordering::Greater => {
+            // Only split the rhs node (which is evaluated first)
+            (*g_level, f, f, g_then, g_else)
+        }
+    }
+}
+
+#[expect(clippy::type_complexity)]
+fn split_on_next_var3<'f, 'g, 'h>(
+    f: &'f Rc<node::Inner>,
+    g: &'g Rc<node::Inner>,
+    h: &'h Rc<node::Inner>,
+) -> (
+    u64,
+    &'f Rc<node::Inner>,
+    &'f Rc<node::Inner>,
+    &'g Rc<node::Inner>,
+    &'g Rc<node::Inner>,
+    &'h Rc<node::Inner>,
+    &'h Rc<node::Inner>,
+) {
+    let node::Kind::NonTerminal {
+        level: f_level,
+        then_child: f_then,
+        else_child: f_else,
+        cache: _,
+    } = &f.kind
+    else {
+        panic!("Expected NonTerminal node for f");
+    };
+    let node::Kind::NonTerminal {
+        level: g_level,
+        then_child: g_then,
+        else_child: g_else,
+        cache: _,
+    } = &g.kind
+    else {
+        panic!("Expected NonTerminal node for g");
+    };
+    let node::Kind::NonTerminal {
+        level: h_level,
+        then_child: h_then,
+        else_child: h_else,
+        cache: _,
+    } = &h.kind
+    else {
+        panic!("Expected NonTerminal node for h");
+    };
+
+    let min_level = f_level.min(g_level).min(h_level);
+
+    let (fv, fnv) = if f_level == min_level {
+        (f_then, f_else)
+    } else {
+        (f, f)
+    };
+    let (gv, gnv) = if g_level == min_level {
+        (g_then, g_else)
+    } else {
+        (g, g)
+    };
+    let (hv, hnv) = if h_level == min_level {
+        (h_then, h_else)
+    } else {
+        (h, h)
+    };
+
+    (*min_level, fv, fnv, gv, gnv, hv, hnv)
 }
 
 #[cfg(test)]
